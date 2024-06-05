6a4f56ce490ad72c3684d1e9205bc7b6e50046cf
xiou555 <408740466@qq.com>
2024-06-05 18:44:05 +0800

    能够转发双手数据到ros--频率不正确，需要更改

--- sub_zmq_transform_ros.py
+++ sub_zmq_transform_ros.py
@@ -7,7 +7,8 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseArray, Pose
 import hydra
-
+import rclpy.time
+from datetime import datetime
 
 VR_FREQ = 60
 class FrequencyTimer(object):
@@ -56,10 +57,10 @@ class ZMQKeypointSubscriber(threading.Thread):
         self.socket.close()
         self.context.term()
 
-class TransformHandPositionCoordsToRos(Node):
-    def __init__(self, host, transformation_port):
-        super().__init__('transform_hand_position_coords_to_ros')
-        print('keypoint transform ros')
+class TransformHandPositionCoordsToRos(Node, threading.Thread):
+    def __init__(self, host, transformation_port, topic_name):
+        super().__init__(topic_name + "_hand")
+        print(topic_name + ' keypoint transform ros')
 
         # 接收到的手部处理后数据
         # 24组数据
@@ -70,25 +71,51 @@ class TransformHandPositionCoordsToRos(Node):
         self.timer = FrequencyTimer(VR_FREQ)
 
         # ros 发布
-        self.hand_coords_publisher_ = self.create_publisher(PoseArray, "transformed_hand_coords", 10)
+        self.hand_coords_publisher_ = self.create_publisher(PoseArray, topic_name + "_transformed_hand_coords", 10)
         self.hand_coords_array = PoseArray()
-        self.hand_coords_array.header.frame_id = "transformed_hand_coords"
+        self.hand_coords_array.header.frame_id = topic_name + "_transformed_hand_coords"
         self.hand_coords_array.header.stamp = self.get_clock().now().to_msg()
 
-        self.hand_matrix_publisher_ = self.create_publisher(PoseArray, "transformed_hand_frame", 10)
+        self.hand_matrix_publisher_ = self.create_publisher(PoseArray, topic_name + "_transformed_hand_frame", 10)
         self.hand_matrix_array = PoseArray()
-        self.hand_matrix_array.header.frame_id = "transformed_hand_frame"
+        self.hand_matrix_array.header.frame_id = topic_name + "_transformed_hand_frame"
         self.hand_matrix_array.header.stamp = self.get_clock().now().to_msg()
+
+        print(topic_name + "_transformed_hand_coords")
+        print(topic_name + "_transformed_hand_frame")
+        self.thread_ = threading.Thread(target = TransformHandPositionCoordsToRos.stream , args=(self,)) 
     
+    def start(self):
+        self.thread_.start()
+
+    def __del__(self):
+        # 等待线程完成任务
+        self.thread_.join()
+
     def stream(self):
         while True:
             try:
+                # print(self.get_clock().now())
                 self.timer.start_loop()
                 hand_coords_data = self.original_hand_coords_subscriber.recv_keypoints()
                 hand_matrix_data = self.original_hand_matrix_subscriber.recv_keypoints()
                 
-                print('transform ros', hand_matrix_data)
-                self.hand_coords_array.header.stamp = self.get_clock().now().to_msg()
+                # 格式化打印时间
+                now = self.get_clock().now()
+                time_msg = now.to_msg()
+                seconds = time_msg.sec
+                nanoseconds = time_msg.nanosec
+                milliseconds = nanoseconds // 1000000  # 转换为毫秒
+                # 将时间戳转换为 datetime 对象
+                time_datetime = datetime.fromtimestamp(seconds)
+                # 格式化时间字符串
+                time_str = time_datetime.strftime("%Y-%m-%d %H:%M:%S")
+                time_str += f".{milliseconds:04d}"
+                self.get_logger().info(f"Current Time: {time_str}")
+                self.rate_.sleep()
+
+                # print('transform ros', hand_matrix_data)
+                self.hand_coords_array.header.stamp = now.to_msg()
                 # 遍历数据并将每行数据添加到 PoseArray 中
                 for row in hand_coords_data:
                     pose = Pose()
@@ -96,13 +123,13 @@ class TransformHandPositionCoordsToRos(Node):
                     self.hand_coords_array.poses.append(pose)
                 self.hand_coords_publisher_.publish(self.hand_coords_array)
                 
-                self.hand_matrix_array.header.stamp = self.get_clock().now().to_msg()
+                self.hand_matrix_array.header.stamp = now.to_msg()
                 for row in hand_matrix_data:
                     pose = Pose()
                     pose.position.x, pose.position.y, pose.position.z = row
                     self.hand_matrix_array.poses.append(pose)
                 self.hand_matrix_publisher_.publish(self.hand_matrix_array)
-                    
+                
                 self.timer.end_loop()
             except:
                 break
@@ -118,11 +145,33 @@ def main(config):
     # host = '192.168.2.58'
     # transformation_port = '8089'
     print(f"Host Address: {config.host_address}")
-    print(f"Transformed Position Left Keypoint Port: {config.transformed_position_keypoint_port}")
+    print(f"Transformed Position right Keypoint Port: {config.transformed_position_keypoint_port}")
+    print(f"Transformed Position left Keypoint Port: {config.transformed_position_left_keypoint_port}")
     host = config.host_address
-    transformation_port = config.transformed_position_keypoint_port
-    while_publisher = TransformHandPositionCoordsToRos(host, transformation_port)
-    while_publisher.stream()
+    right_transformation_port = config.transformed_position_keypoint_port
+    left_transformation_port = config.transformed_position_left_keypoint_port
+    # right_while_publisher = TransformHandPositionCoordsToRos(host, right_transformation_port, 'right')
+    # right_while_publisher.stream()
+    # left_while_publisher = TransformHandPositionCoordsToRos(host, left_transformation_port, 'left')
+    # left_while_publisher.stream()
+
+    # Create a list to store threads
+    threads = []
+
+    threads.append(TransformHandPositionCoordsToRos(host, right_transformation_port, 'right'))
+    threads.append(TransformHandPositionCoordsToRos(host, left_transformation_port, 'left'))
+    for thread in threads:
+        thread.start()
+
+    while rclpy.ok():
+        time.sleep(1)
+
+    # 主循环退出后关闭节点
+    # right_while_publisher.destroy_node()
+    # left_while_publisher.destroy_node()
+    # for thread in threads:
+    #     thread.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
